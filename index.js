1b21a19e7454edc5993e71ffc224de21b938b57f
Rabyn-Collab <babynshrestha64@gmail.com>
2025-11-06 08:50:27 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// primitive types , refrence types
+// pri--- String , Number , boolean, undefined, null,
+//ref --- array, object, function
+
+
+
+const d = new Date();
+
+// console.log(d.getDay());
+// console.log(d.getDate());
+// console.log(d.getTimezoneOffset());
+// console.log(d.toISOString());
+// console.log(d.getTime());
+// console.log(d.getHours());
+console.log(Date.now());
+
+
+
