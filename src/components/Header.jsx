913d1e4037c4a021049f1f8ffd5c4b3737e837e1
Rabyn-Collab <babynshrestha64@gmail.com>
2025-11-10 09:59:56 +0545

    changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router";
+import { Button } from "./ui/button";
+
+export default function Header() {
+  return (
+    <div className="bg-gray-200 px-5 flex justify-between  items-end py-2">
+      <h1 className="text-[30px] font-bold ">Shopal</h1>
+
+      <div className="space-x-5">
+        <NavLink to={'/login'}>
+          <Button variant="link" className={'text-[16px]'}>Login</Button>
+        </NavLink>
+
+        <Button>Sign Up</Button>
+      </div>
+      {/* <DropDownProfile /> */}
+
+    </div>
+  )
+}
+
+
+
---
+++ src/components/RootLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+import Header from './Header'
+
+export default function RootLayout() {
+  return (
+    <>
+
+      <Header />
+
+      <Outlet />
+    </>
+  )
+}
