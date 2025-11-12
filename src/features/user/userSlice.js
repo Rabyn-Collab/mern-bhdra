039b9d0f58f6b753fbb3278d76d21c696f9c7d56
Rabyn-Collab <babynshrestha64@gmail.com>
2025-11-12 10:01:19 +0545

    changes

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,14 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getUserFromLocal, setUserToLocal } from "../local/local";
 
 
 
 
 export const userSlice = createSlice({
   name: 'userSlice',
+  initialState: {
+    user: getUserFromLocal(),
+  },
   reducers: {
 
     setUser: (state, action) => {
-
+      state.user = action.payload;
+      setUserToLocal(action.payload);
     },
 
   }
