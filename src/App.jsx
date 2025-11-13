b997d5e64f941745799a4ee9935ab814447dbce7
Rabyn-Collab <babynshrestha64@gmail.com>
2025-11-13 08:52:41 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
 import NotFound from "./pages/not-found/NotFound";
-import Page1 from "./pages/home/nested-pages/Page1";
-import Page2 from "./pages/home/nested-pages/Page2";
+import RootLayout from "./components/RootLayout";
 
 //tailwind css // routing basic concept
 
@@ -15,26 +14,24 @@ export default function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Home />,
+      element: <RootLayout />,
       children: [
         {
-          path: 'page-1',
-          element: <Page1 />
+          index: true,
+          element: <Home />
+        },
+
+        {
+          path: 'about',
+          element: <About />
         },
         {
-          path: 'page-2',
-          element: <Page2 />
-        }
+          path: 'contact',
+          element: <Contact />
+        },
       ]
     },
-    {
-      path: 'about',
-      element: <About />
-    },
-    {
-      path: 'contact',
-      element: <Contact />
-    },
+
 
     {
       path: '*',
--- src/pages/home/nested-pages/Page1.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Page1() {
-  return (
-    <div>
-      <h1>This is page 1 </h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat laudantium veritatis possimus alias laborum fuga, distinctio quasi iusto, esse cupiditate amet repudiandae ipsa? Tenetur vero ducimus iure nisi expedita soluta ratione rem porro ipsum, incidunt suscipit architecto blanditiis? Aut ducimus eius numquam quis expedita, aspernatur obcaecati minus! Sed aperiam temporibus eos, quisquam iusto illo quos ullam mollitia quod vero cupiditate dolorem libero, debitis, deserunt cum ut incidunt possimus rem impedit expedita molestiae labore placeat obcaecati laboriosam. Dolorum repellendus sunt velit unde ab quaerat, earum, in minus sint architecto, harum nemo officia consectetur doloremque quod quos esse. Facilis eum pariatur odit.</p>
-    </div>
-  )
-}
