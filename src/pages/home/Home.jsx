22af28365daf95de56688121e31dd6c97be8a478
Rabyn-Collab <babynshrestha64@gmail.com>
2025-12-05 08:06:06 +0545

    css create push

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,33 +1,11 @@
-import { memo, useCallback, useMemo, useState } from "react";
-import { Input } from "../../components/ui/input";
-import { Button } from "../../components/ui/button";
-import ChildComponent from "./ChildComponent";
+
 
 
 export default function Home() {
-
-  const [count, setCount] = useState(1);
-
-  const func = () => {
-    alert('hello');
-  };
-
-
-
   return (
-    <div className="max-w-sm">
+    <div>
 
-
-      {/* 
-      <Input placeholder="Search" /> */}
-
-      <h1>{count}</h1>
-      <Button onClick={() => setCount(count + 1)} >Increment</Button>
-      <ChildComponent func={func} />
 
     </div>
   )
 }
-
-
-
