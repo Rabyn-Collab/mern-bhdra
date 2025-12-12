e19053e947fa3ddc594ca97823d96a1b49a592ef
Rabyn-Collab <babynshrestha64@gmail.com>
2025-12-12 09:59:56 +0545

    changes new

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,21 @@
+
+export default function PostsPage() {
+
+  const posts = [
+    { id: 1, title: 'hello' },
+    { id: 2, title: 'sello' },
+  ];
+  return (
+    <div>
+
+
+      {posts.map((post) => {
+        return <h1 key={post.id}>{post.title}</h1>
+      })}
+
+
+
+
+    </div>
+  )
+}
