e6ca0074387da52e788a9610adbe62ea1af053eb
Rabyn-Collab <babynshrestha64@gmail.com>
2026-02-12 08:25:39 +0545

    changes

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -2,8 +2,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
 
-export const baseUrl = 'http://192.168.100.6:5000/api';
-export const base = 'http://192.168.100.6:5000';
+// export const baseUrl = 'http://192.168.100.6:5000/api';
+// export const base = 'http://192.168.100.6:5000';
+
+
+export const baseUrl = 'https://shop-puax.onrender.com/api';
+export const base = 'https://shop-puax.onrender.com';
 
 export const mainApi = createApi({
   reducerPath: 'mainApi',
